a617e331580947158cc72c82ed56340eddd77e6a
Hevsy <roomyn@gmail.com>
2023-08-14 22:24:01 +1000

    WIP: Adding vpc and ec2 isntanse

--- dev/main.tf
+++ dev/main.tf
@@ -3,34 +3,56 @@
 # be bootstrapped according to the simple yet essential procedure in
 # https://github.com/cloudposse/terraform-aws-tfstate-backend#usage
 module "terraform_state_backend" {
-    source = "github.com/Hevsy/terraform-modules//terraform-aws-tfstate-backend?ref=v1.0.0"
-    # Cloud Posse recommends pinning every module to a specific version
-    # version     = "1.0.0"
-    namespace  = "ppv"
-    stage      = "dev"
-    name       = "terraform"
-    attributes = ["state"]
+  source = "github.com/Hevsy/terraform-modules//terraform-aws-tfstate-backend?ref=v1.0.0"
+  # Cloud Posse recommends pinning every module to a specific version
+  # version     = "1.0.0"
+  namespace  = "ppv"
+  stage      = "dev"
+  name       = "terraform"
+  attributes = ["state"]
 
-    terraform_backend_config_file_path = "."
-    terraform_backend_config_file_name = "backend.tf"
-    force_destroy                      = false
+  terraform_backend_config_file_path = "."
+  terraform_backend_config_file_name = "backend.tf"
+  force_destroy                      = false
 }
 
 # Your Terraform configuration
 module "vpc" {
-    source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.1"
 
-    name = "ddbp-vpn"
-    cidr = "10.123.0.0/24"
+  name = "ddbp-vpn"
+  cidr = "10.123.0.0/24"
 
-    azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-    public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs            = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  public_subnets = ["10.123.101.0/24"]
+  intra_subnets  = ["10.123.51.0/24"]
 
-    enable_nat_gateway = false
-    enable_vpn_gateway = false
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
 
-    tags = {
-    Terraform = "true"
+  tags = {
+    Terraform   = "true"
     Environment = "dev"
-    }
+    app         = "${var.app}"
+  }
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.2.1"
+
+  name = "single-instance"
+
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    app         = "${var.app}"
+  }
 }
